6dcdd78030d23cb58bf9233a67dc8fe0008d4ff7
Divya <divya.parameswaran@digital.cabinet-office.gov.uk>
2023-07-17 13:45:17 +0100

    OLH-922 Add implemetation for userinfo stub

---
+++ oidc/userinfo/oidc-userinfo-stub.ts
@@ -0,0 +1,23 @@
+import { UserInfo } from "./models";
+import { v4 as uuid } from "uuid";
+
+export interface Response {
+  statusCode: number;
+  body: UserInfo;
+}
+
+const newUserInfo = (): UserInfo => ({
+  sub: uuid(),
+  email: "test@test.com",
+  email_verified: true,
+  phone: "1234567890",
+  phone_verified: true,
+  updated_at: Date.now.toString(),
+});
+
+export const handler = async () => {
+  return {
+    statusCode: 200,
+    body: newUserInfo(),
+  };
+};
