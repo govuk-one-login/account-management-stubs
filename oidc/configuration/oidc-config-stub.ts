8a6c888f9c1390f513bb089a6db40ccf5dc20a25
Dougal Rees <24165331+DougsCER@users.noreply.github.com>
2023-07-18 11:23:55 +0100

    OIDC Config Stub

---
+++ oidc/configuration/oidc-config-stub.ts
@@ -0,0 +1,59 @@
+export interface Response {
+  statusCode: number;
+  body: string;
+}
+
+export const handler = async () => {
+  const { ENVIRONMENT } = process.env;
+
+  const configurationData = {
+    authorization_endpoint: `https://oidc-stub.home.${ENVIRONMENT}.account.gov.uk/authorize`,
+    token_endpoint: `https://oidc-stub.home.${ENVIRONMENT}.account.gov.uk/token`,
+    registration_endpoint: `https://oidc-stub.home.${ENVIRONMENT}.account.gov.uk/connect/register`,
+    issuer: `https://oidc-stub.home.${ENVIRONMENT}.account.gov.uk/`,
+    jwks_uri: `https://oidc-stub.home.${ENVIRONMENT}.account.gov.uk/.well-known/jwks.json`,
+    scopes_supported: ["openid", "email", "phone", "offline_access"],
+    response_types_supported: ["code"],
+    grant_types_supported: ["authorization_code"],
+    token_endpoint_auth_methods_supported: ["private_key_jwt"],
+    token_endpoint_auth_signing_alg_values_supported: [
+      "RS256",
+      "RS384",
+      "RS512",
+      "PS256",
+      "PS384",
+      "PS512",
+    ],
+    ui_locales_supported: ["en", "cy"],
+    service_documentation: "https://unsuported-by-stub.gov.uk/",
+    op_policy_uri: "https://unsuported-by-stub.gov.uk/",
+    op_tos_uri: "https://unsuported-by-stub.gov.uk/",
+    request_uri_parameter_supported: true,
+    trustmarks: "https://unsuported-by-stub.gov.uk/",
+    subject_types_supported: ["public", "pairwise"],
+    userinfo_endpoint: `https://oidc-stub.home.${ENVIRONMENT}.account.gov.uk/userinfo`,
+    end_session_endpoint: "https://unsuported-by-stub.gov.uk/",
+    id_token_signing_alg_values_supported: ["ES256", "RS256"],
+    claim_types_supported: ["normal"],
+    claims_supported: [
+      "sub",
+      "email",
+      "email_verified",
+      "phone_number",
+      "phone_number_verified",
+      "https://vocab.account.gov.uk/v1/passport",
+      "https://vocab.account.gov.uk/v1/drivingPermit",
+      "https://vocab.account.gov.uk/v1/coreIdentityJWT",
+      "https://vocab.account.gov.uk/v1/address",
+    ],
+    backchannel_logout_supported: true,
+    backchannel_logout_session_supported: false,
+  };
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(configurationData),
+  };
+
+  return response;
+};
