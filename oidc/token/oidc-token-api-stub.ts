8dbcee59fbeb230e9657b6f6ce4c71758ee3703a
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2023-07-25 11:09:26 +0100

    prettier

--- oidc/token/oidc-token-api-stub.ts
+++ oidc/token/oidc-token-api-stub.ts
@@ -1,5 +1,11 @@
 import { v4 as uuid } from "uuid";
-import { IdToken, JwtHeader, Response, TokenResponse, UserInfo } from "./models";
+import {
+  IdToken,
+  JwtHeader,
+  Response,
+  TokenResponse,
+  UserInfo,
+} from "./models";
 import * as jwt from "jsonwebtoken";
 const { sign } = jwt;
 
@@ -17,52 +23,52 @@ const newClaims = (OIDC_CLIENT_ID: string): IdToken => ({
   iss: "https://oidc-stub.home.account.gov.uk",
   nonce: "to fill in", // get from input params
   aud: OIDC_CLIENT_ID,
-  exp: epochDateNow() +  3600,
+  exp: epochDateNow() + 3600,
   iat: epochDateNow(),
   sid: uuid(),
 });
 
 const newJwtHeader = (): JwtHeader => ({
-  kid: "to fill in",  /// insert key identifier when key has been made
-   alg:  "ES256",
-})
+  kid: "to fill in", /// insert key identifier when key has been made
+  alg: "ES256",
+});
 
-const epochDateNow = (): number => (
-  Math.round(Date.now() / 1000)
-);
+const epochDateNow = (): number => Math.round(Date.now() / 1000);
 
 export const handler = async (): Promise<Response> => {
-  const { OIDC_CLIENT_ID } = process.env
+  const { OIDC_CLIENT_ID } = process.env;
 
-  if (
-    typeof OIDC_CLIENT_ID === "undefined"
-  ) {
-    throw new Error(
-      "OIDC_CLIENT_ID environemnt variable is null"
-    );
+  if (typeof OIDC_CLIENT_ID === "undefined") {
+    throw new Error("OIDC_CLIENT_ID environemnt variable is null");
   }
 
   const jwtHeader: JwtHeader = newJwtHeader();
-  const headerEncoded: string = Buffer.from(JSON.stringify(jwtHeader)).toString("base64");
+  const headerEncoded: string = Buffer.from(JSON.stringify(jwtHeader)).toString(
+    "base64"
+  );
   const claims = newClaims(OIDC_CLIENT_ID);
-  const claimsEncoded: string = Buffer.from(JSON.stringify(claims)).toString("base64"); 
-  
-  const token = sign(claims, "kMHcCAQEEIF1b0TZRHZGsl/YHQ2tUs8ldvPdTOwrtXH9y13mm/YDdoAoGCCqGSM49AwEHoUQDQgAEW6/i8ocQVza/SJuVMud319gr1NURxzo6/OSCPlCa9CqnUUAGy+A0wx1A5YfzhVyhhm7FR3nI4q6R2gzLZqzKXA==ey", {algorithm: 'ES256'});
-  
+  const claimsEncoded: string = Buffer.from(JSON.stringify(claims)).toString(
+    "base64"
+  );
+
+  const token = sign(
+    claims,
+    "kMHcCAQEEIF1b0TZRHZGsl/YHQ2tUs8ldvPdTOwrtXH9y13mm/YDdoAoGCCqGSM49AwEHoUQDQgAEW6/i8ocQVza/SJuVMud319gr1NURxzo6/OSCPlCa9CqnUUAGy+A0wx1A5YfzhVyhhm7FR3nI4q6R2gzLZqzKXA==ey",
+    { algorithm: "ES256" }
+  );
+
   console.log("token: " + token);
 
   const tokenResponse = (): TokenResponse => ({
     access_token: "123ABC",
     refresh_token: "456DEF",
-    token_type: "Bearer", 
+    token_type: "Bearer",
     expires_in: 3600,
-    id_token: "TBC", 
-  })
+    id_token: "TBC",
+  });
 
   return {
     statusCode: 200,
     body: JSON.stringify(tokenResponse),
   };
 };
-
-
