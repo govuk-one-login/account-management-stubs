a0653d3cf8f0daf0b5d63a5f0fa5e086f5f2d1e3
Saral Kaushik <saral.kaushik@digital.cabinet-office.gov.uk>
2024-04-03 10:57:53 +0100

    validate

--- src/method-management/method-management.ts
+++ src/method-management/method-management.ts
@@ -1,4 +1,6 @@
+import { APIGatewayProxyEvent } from "aws-lambda";
 import { components } from "./models/schema";
+import assert from 'node:assert/strict'
 
 type MfaMethod = components["schemas"]["MfaMethod"];
 
@@ -24,7 +26,21 @@ export const userInfoHandler = async (): Promise<Response> => {
   };
 };
 
-export const createMfaMethodHandler = async (): Promise<Response> => {
+export const createMfaMethodHandler = async (event: APIGatewayProxyEvent): Promise<Response> => {
+  const { email, otp, credential, mfaMethod: {priorityIdentifier, mfaMethodType}} = JSON.parse(event.body || '{}');
+  try {
+    assert(email, 'no email provided')
+    assert(otp, 'no otp provided')
+    assert(credential, 'no credential provided')
+    assert.match(priorityIdentifier, /^(PRIMARY|SECONDARY)$/, 'invalid priorityIdentifier')
+    assert.match(mfaMethodType, /^(AUTH_APP|SMS)$/, 'invalid mfaMethodType')
+  } catch(e) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({error: (e as Error).message}),
+    };
+  }
+
   return {
     statusCode: 200,
     body: JSON.stringify({}),
