8cfe39ec5081f9cd50e1db7676f043cfb8a83a3e
Saral Kaushik <saral.kaushik@digital.cabinet-office.gov.uk>
2024-04-30 12:04:13 +0100

    write user id to table

--- src/tests/oidc/userinfo.test.ts
+++ src/tests/oidc/userinfo.test.ts
@@ -2,7 +2,10 @@ import { handler, Response } from "../../oidc/userinfo";
 
 describe("handler", () => {
   test("returns status code 200", async () => {
-    const result: Response = await handler();
+    const mockApiEvent: APIGatewayProxyEvent = {
+      body: "client_assertion_type=urn%3Aietf%3Aparams%3Aoauth%3Aclient-assertion-type%3Ajwt-bearer&client_assertion=eyJhbGkpXVCJ9.ey5BPMzRJIn0.RmHvYkaw&grant_type=authorization_code&code=ccca4dec-6799-413c-ab45-896d050006b5&redirect_uri=https%3A%2F%2Fhome.dev.account.gov.uk%2Fauth%2Fcallback",
+    } as never;
+    const result: Response = await handler(mockApiEvent);
     expect(result.statusCode).toEqual(200);
   });
 });
