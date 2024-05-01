2129ddedbfc93a69e6939b8e4a9374ea9614078f
Saral Kaushik <saral.kaushik@digital.cabinet-office.gov.uk>
2024-05-01 11:34:58 +0100

    add keyschema

--- src/oidc/userinfo.ts
+++ src/oidc/userinfo.ts
@@ -3,6 +3,7 @@ import { DynamoDBDocumentClient, QueryCommand } from "@aws-sdk/lib-dynamodb";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import assert from "node:assert/strict";
 import { UserInfo } from "../common/models";
+import getUserScenario from "../scenarios/scenarios";
 
 export interface Response {
   statusCode: number;
@@ -56,15 +57,6 @@ const parseJwt = (
   return JSON.parse(Buffer.from(token.split(".")[1], "base64").toString());
 };
 
-const newUserInfo = (userId: string): UserInfo => ({
-  sub: userId,
-  email: "your.name@example.com",
-  email_verified: true,
-  phone_number: "1234567890",
-  phone_number_verified: true,
-  updated_at: Date.now().toString(),
-});
-
 export const handler = async (
   event: APIGatewayProxyEvent
 ): Promise<Response> => {
@@ -73,11 +65,12 @@ export const handler = async (
     "There is no Authorization header in the request"
   );
 
-  console.log("event", event);
   const jwt = parseJwt(event.headers.Authorization);
 
   return {
     statusCode: 200,
-    body: JSON.stringify(newUserInfo(await getUserId(jwt.nonce))),
+    body: JSON.stringify(
+      getUserScenario(await getUserId(jwt.nonce), "userinfo")
+    ),
   };
 };
---
+++ src/scenarios/scenarios.ts
@@ -0,0 +1,53 @@
+import assert from "node:assert/strict";
+
+interface UserScenarios {
+  [key: string]: {
+    userinfo: {
+      email: string;
+      email_verified: boolean;
+      sub: string;
+      phone_number: string;
+      phone_number_verified: boolean;
+      updated_at: string;
+    };
+  };
+}
+
+type NestedKeys<T> = keyof T[keyof T];
+
+const userScenarios: UserScenarios = {
+  "F5CE808F-75AB-4ECD-BBFC-FF9DBF5330FA": {
+    userinfo: {
+      sub: "F5CE808F-75AB-4ECD-BBFC-FF9DBF5330FA",
+      email: "your.name@example.com",
+      email_verified: true,
+      phone_number: "1234567890",
+      phone_number_verified: true,
+      updated_at: Date.now().toString(),
+      d,
+    },
+  },
+  user1: {
+    userinfo: {
+      email: "user1@example.com",
+      email_verified: true,
+      sub: "user1",
+      phone_number: "999",
+      phone_number_verified: true,
+      updated_at: Date.now().toString(),
+    },
+  },
+};
+
+const getUserScenario = (
+  userId: keyof typeof userScenarios,
+  scenario: NestedKeys<UserScenarios>
+) => {
+  assert(
+    userScenarios[userId] && userScenarios[userId][scenario],
+    `scenario "${scenario}" does not exist for user "${userId}"`
+  );
+
+  return userScenarios[userId][scenario];
+};
+export default getUserScenario;
