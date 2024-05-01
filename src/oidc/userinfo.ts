535c95e13d6681451d303439dda0d29ccade24b4
Saral Kaushik <saral.kaushik@digital.cabinet-office.gov.uk>
2024-05-01 08:24:44 +0100

    get custom user id

--- src/oidc/userinfo.ts
+++ src/oidc/userinfo.ts
@@ -1,4 +1,7 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
+import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import assert from "node:assert/strict";
 import { UserInfo } from "../common/models";
 
 export interface Response {
@@ -6,12 +9,50 @@ export interface Response {
   body: string;
 }
 
-const parseJwt = (token: string) => {
+interface OicdPersistedData {
+  code: string;
+  nonce: string;
+  userId: string;
+}
+
+const marshallOptions = {
+  convertClassInstanceToMap: true,
+};
+const translateConfig = { marshallOptions };
+const dynamoClient = new DynamoDBClient({});
+const dynamoDocClient = DynamoDBDocumentClient.from(
+  dynamoClient,
+  translateConfig
+);
+
+const getUserId = async (nonce: string): Promise<string> => {
+  const { TABLE_NAME } = process.env;
+  const command = new GetCommand({
+    TableName: TABLE_NAME,
+    Key: {
+      nonce,
+    },
+  });
+  const results = await dynamoDocClient.send(command);
+  if (results.Item === undefined) {
+    throw new Error("nonce not found in DB");
+  }
+  return (
+    (results.Item as OicdPersistedData).userId ||
+    "F5CE808F-75AB-4ECD-BBFC-FF9DBF5330FA"
+  );
+};
+
+const parseJwt = (
+  token: string
+): {
+  nonce: string;
+} => {
   return JSON.parse(Buffer.from(token.split(".")[1], "base64").toString());
 };
 
-const newUserInfo = (): UserInfo => ({
-  sub: "F5CE808F-75AB-4ECD-BBFC-FF9DBF5330FA",
+const newUserInfo = (userId: string): UserInfo => ({
+  sub: userId,
   email: "your.name@example.com",
   email_verified: true,
   phone_number: "1234567890",
@@ -22,14 +63,13 @@ const newUserInfo = (): UserInfo => ({
 export const handler = async (
   event: APIGatewayProxyEvent
 ): Promise<Response> => {
-  console.log("event", event);
+  assert(event?.headers?.Authorization);
 
-  if (event?.headers?.Authorization) {
-    console.log("jwt", parseJwt(event?.headers?.Authorization));
-  }
+  console.log("event", event);
+  const jwt = parseJwt(event?.headers?.Authorization);
 
   return {
     statusCode: 200,
-    body: JSON.stringify(newUserInfo()),
+    body: JSON.stringify(newUserInfo(await getUserId(jwt.nonce))),
   };
 };
