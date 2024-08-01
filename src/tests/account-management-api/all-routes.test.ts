e0d48989c1514585fd66dd895775a7312755d87f
Saral Kaushik <saral.kaushik@digital.cabinet-office.gov.uk>
2024-08-01 09:18:40 +0100

    add tests

--- src/tests/account-management-api/all-routes.test.ts
+++ src/tests/account-management-api/all-routes.test.ts
@@ -1,8 +1,30 @@
+import { APIGatewayEventDefaultAuthorizerContext, APIGatewayEventRequestContextWithAuthorizer, APIGatewayProxyEvent } from "aws-lambda";
 import { handler, Response } from "../../account-management-api/all-routes";
+
+const createFakeAPIGatewayProxyEvent = (
+  body: unknown,
+  path: string
+): APIGatewayProxyEvent => {
+  return {
+    body: JSON.stringify(body),
+    httpMethod: "PUT",
+    path: path,
+    pathParameters: {},
+    isBase64Encoded: false,
+    headers: {},
+    multiValueHeaders: {},
+    queryStringParameters: null,
+    multiValueQueryStringParameters: null,
+    stageVariables: null,
+    requestContext:
+      {} as APIGatewayEventRequestContextWithAuthorizer<APIGatewayEventDefaultAuthorizerContext>,
+    resource: "",
+  };
+};
 
 describe("handler", () => {
   test("returns status code 204", async () => {
-    const result: Response = await handler();
+    const result: Response = await handler(createFakeAPIGatewayProxyEvent({}, 'test'));
     expect(result.statusCode).toEqual(204);
   });
 });
