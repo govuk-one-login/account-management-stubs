463fc51dced2864415c51d7ff2a0eb56d8d45240
Latif <latif.muhammad@digital.cabinet-office.gov.uk>
2025-01-20 14:28:34 +0000

    Revert "Revert "OLH-2205-performance-testing-implementation-of-overload-protection-library-causing-http-503-errors-optimise-stub-memory" [skip]" (#387)
    Reverts govuk-one-login/account-management-stubs#379

--- .husky/_/husky.sh
+++ .husky/_/husky.sh
@@ -1,1 +1,9 @@
-echo "husky - `#!/usr/bin/env sh` and `. "$(dirname -- "$0")/_/husky.sh"` lines in hooks are DEPRECATED and won't be supported in v10. You can remove these two lines for even simpler scripts"
+echo "husky - DEPRECATED
+
+Please remove the following two lines from $0:
+
+#!/usr/bin/env sh
+. \"\$(dirname -- \"\$0\")/_/husky.sh\"
+
+They WILL FAIL in v10.0.0
+"
--- src/tests/oidc/jkws.test.ts
+++ src/tests/oidc/jkws.test.ts
@@ -1,3 +1,5 @@
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.test" });
 import { Response, handler } from "../../oidc/jwks";
 
 describe("handler", () => {
