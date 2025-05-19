c2ef2cf3a7a580859d51152213314e331b6106c6
Alex Whitehead-Smith <alex.whitehead-smith@digital.cabinet-office.gov.uk>
2025-05-19 15:39:02 +0100

    OLH-2655: Handle OIDC logout requests
    'Handle' OIDC logout requests that include a `post_logout_redirect_uri`
    parameter by validating the domain and simply redirecting the user back
    to that URI.
    
    This is compliant with the logout interface described in [our public
    tech documentation](https://docs.sign-in.service.gov.uk/integrate-with-integration-environment/managing-your-users-sessions/#make-a-request-to-log-your-user-out-of-gov-uk-one-login).
    
    We need to do the domain validation here to protect ourselves from an
    [open redirect attack](https://hacksplaining.com/prevention/open-redirects).
    We need to be extra carfeul here as we allow redirects to `localhost`.
    If an attacker's got a webserver running on a user's machine then that
    user is already having a bad day. We need to do as much as we can to
    prevent phishing links off the `*.gov.uk` domain which is why I've also
    added the referrer check.

---
+++ src/oidc/logout.ts
@@ -0,0 +1,54 @@
+import { APIGatewayProxyEvent } from "aws-lambda";
+import { RedirectResponse } from "../common/response-utils";
+import assert from "node:assert/strict";
+
+const VALID_HOSTNAMES = [
+  "home.dev.account.gov.uk",
+  "home.build.account.gov.uk",
+  "localhost",
+];
+
+export const validateRedirectUri = (redirectUri: string) => {
+  const url = new URL(redirectUri);
+  if (!VALID_HOSTNAMES.includes(url.hostname)) {
+    throw new Error("Redirect URI must be to an allowed domain");
+  }
+
+  if (url.hostname === "localhost" && url.protocol === "http:") {
+    return;
+  }
+
+  if (url.protocol != "https:") {
+    throw new Error("Redirect URI protocol must be HTTPS if not localhost");
+  }
+};
+
+export const validateReferrer = (referrer: string) => {
+  const url = new URL(referrer);
+  if (!VALID_HOSTNAMES.includes(url.hostname)) {
+    throw new Error("Referrer must be an allowed domain");
+  }
+};
+
+export const handler = async (
+  event: APIGatewayProxyEvent
+): Promise<RedirectResponse> => {
+  assert(event.queryStringParameters, "No query parameters provided");
+
+  const token = event.queryStringParameters.id_token_hint;
+  const redirectUri = event.queryStringParameters.post_logout_redirect_uri;
+  const referrer = event.headers.Referer;
+
+  assert(token, "No id_token_hint provided");
+  assert(redirectUri, "No post_logout_redirect_uri provided");
+  assert(referrer, "No Origin header");
+
+  validateRedirectUri(redirectUri);
+  validateReferrer(referrer);
+  return {
+    statusCode: 302,
+    headers: {
+      Location: redirectUri,
+    },
+  };
+};
---
+++ src/tests/oidc/logout.test.ts
@@ -0,0 +1,143 @@
+import {
+  handler,
+  validateRedirectUri,
+  validateReferrer,
+} from "../../oidc/logout";
+import { APIGatewayProxyEvent } from "aws-lambda";
+
+const VALID_REDIRECT_URI = "https://home.dev.account.gov.uk/logout";
+
+describe(validateRedirectUri, () => {
+  test("throws an error with a malformed URI", () => {
+    expect(() => {
+      validateRedirectUri("Not a URL");
+    }).toThrow();
+  });
+
+  test("throws an error when the domain is not allowed", () => {
+    expect(() => {
+      validateRedirectUri("https://example.com/logout");
+    }).toThrow();
+  });
+
+  test("throws an error when domain is valid but not HTTPS", () => {
+    expect(() => {
+      validateRedirectUri("http://home.dev.account.gov.uk/logout");
+    }).toThrow();
+  });
+
+  test("doesn't throw an error when redirecting to localhost on HTTP", () => {
+    expect(() => {
+      validateRedirectUri("http://localhost/logout");
+    }).not.toThrow();
+  });
+
+  test("doesn't throw an error when redirecting to a valid domain on https", () => {
+    expect(() => {
+      validateRedirectUri(VALID_REDIRECT_URI);
+    }).not.toThrow();
+  });
+});
+
+describe(validateReferrer, () => {
+  test("throws an error with a malformed URI", () => {
+    expect(() => {
+      validateReferrer("Not a URL");
+    }).toThrow();
+  });
+
+  test("throws an error when the domain is not allowed", () => {
+    expect(() => {
+      validateReferrer("https://example.com/logout");
+    }).toThrow();
+  });
+
+  test("doesn't throw an error when referrer is localhost", () => {
+    expect(() => {
+      validateReferrer("http://localhost/logout");
+    }).not.toThrow();
+  });
+
+  test("doesn't throw an error when redirecting to a valid domain on https", () => {
+    expect(() => {
+      validateReferrer(VALID_REDIRECT_URI);
+    }).not.toThrow();
+  });
+});
+
+describe(handler, () => {
+  test("returns a redirect response from a valid request", async () => {
+    const event: Partial<APIGatewayProxyEvent> = {
+      queryStringParameters: {
+        post_logout_redirect_uri: VALID_REDIRECT_URI,
+        id_token_hint: "id-token",
+      },
+      headers: {
+        Referer: VALID_REDIRECT_URI,
+      },
+    };
+    const response = await handler(event as APIGatewayProxyEvent);
+    expect(response.statusCode).toBe(302);
+    expect(response.headers.Location).toBe(VALID_REDIRECT_URI);
+  });
+
+  test("throws an error when ID token is missing", async () => {
+    const event: Partial<APIGatewayProxyEvent> = {
+      queryStringParameters: {
+        post_logout_redirect_uri: VALID_REDIRECT_URI,
+      },
+      headers: {
+        Referer: VALID_REDIRECT_URI,
+      },
+    };
+    expect(handler(event as APIGatewayProxyEvent)).rejects.toThrow();
+  });
+
+  test("throws an error when redirect URI is missing", async () => {
+    const event: Partial<APIGatewayProxyEvent> = {
+      queryStringParameters: {
+        id_token_hint: "id-token",
+      },
+      headers: {
+        Referer: VALID_REDIRECT_URI,
+      },
+    };
+    expect(handler(event as APIGatewayProxyEvent)).rejects.toThrow();
+  });
+
+  test("throws an error when redirect URI is not on the allow list", async () => {
+    const event: Partial<APIGatewayProxyEvent> = {
+      queryStringParameters: {
+        post_logout_redirect_uri: "https://example.com/logout",
+        id_token_hint: "id-token",
+      },
+      headers: {
+        Referer: VALID_REDIRECT_URI,
+      },
+    };
+    expect(handler(event as APIGatewayProxyEvent)).rejects.toThrow();
+  });
+
+  test("throws an error when referrer header is missing", async () => {
+    const event: Partial<APIGatewayProxyEvent> = {
+      queryStringParameters: {
+        post_logout_redirect_uri: VALID_REDIRECT_URI,
+        id_token_hint: "id-token",
+      },
+    };
+    expect(handler(event as APIGatewayProxyEvent)).rejects.toThrow();
+  });
+
+  test("throws an error when referrer is not on the allow list", async () => {
+    const event: Partial<APIGatewayProxyEvent> = {
+      queryStringParameters: {
+        post_logout_redirect_uri: VALID_REDIRECT_URI,
+        id_token_hint: "id-token",
+      },
+      headers: {
+        Referer: "https://example.com/logout",
+      },
+    };
+    expect(handler(event as APIGatewayProxyEvent)).rejects.toThrow();
+  });
+});
