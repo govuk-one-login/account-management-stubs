570efcf094851ed08b1e73bc11b06bca8e99a96d
latifcabinetoffice <latif.muhammad@digital.cabinet-office.gov.uk>
2025-06-24 11:59:09 +0100

    OLH-2808: replace TXMA scenario with user ID

--- src/oidc/authorize.ts
+++ src/oidc/authorize.ts
@@ -40,13 +40,13 @@ export interface Response {
   };
 }
 
-const newTxmaEvent = (user_id: string): TxmaEvent => ({
+const newTxmaEvent = (userId: string): TxmaEvent => ({
   event_id: uuid(),
   timestamp: Date.now(),
   event_name: "AUTH_AUTH_CODE_ISSUED",
   client_id: "vehicleOperatorLicense",
   user: {
-    user_id: user_id,
+    user_id: userId,
     session_id: uuid(),
   },
 });
@@ -66,8 +66,8 @@ export const sendSqsMessage = async (
 export const writeNonce = async (
   code: string,
   nonce: string,
-  userId = "urn:fdc:gov.uk:default",
-  remove_at: number
+  remove_at: number,
+  userId = "urn:fdc:gov.uk:default"
 ): Promise<PutCommandOutput> => {
   const command = new PutCommand({
     TableName: TABLE_NAME,
@@ -144,10 +144,11 @@ export const handler = async (
   );
 
   try {
+    const userId = `urn:fdc:gov.uk:${scenario}`;
     await Promise.all([
-      writeNonce(code, nonce, scenario, remove_at),
+      writeNonce(code, nonce, remove_at, userId),
       sendSqsMessage(
-        JSON.stringify(newTxmaEvent(scenario)),
+        JSON.stringify(newTxmaEvent(userId)),
         DUMMY_TXMA_QUEUE_URL
       ),
     ]);
