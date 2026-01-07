c9e6dbfd34f721b0026e039e4dd8ec2459680073
Dana Cotoran <dana.cotoran@digital.cabinet-office.gov.uk>
2026-01-07 18:25:04 +0000

    OLH-3359: Tweak select scenario handler to support request object query string parameter

--- src/oidc/authorize.ts
+++ src/oidc/authorize.ts
@@ -1,5 +1,6 @@
 import { randomUUID } from "node:crypto";
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { decodeJwt } from "jose";
 import {
   DynamoDBDocumentClient,
   PutCommand,
@@ -84,9 +85,18 @@ export const writeNonce = async (
 export const selectScenarioHandler = async (event: APIGatewayProxyEvent) => {
   const queryStringParameters: APIGatewayProxyEventQueryStringParameters =
     event.queryStringParameters as APIGatewayProxyEventQueryStringParameters;
-
-  const { state, nonce, redirect_uri } = queryStringParameters;
-
+  let mockState, mockNonce, mockRedirectUri;
+  const { state, nonce, redirect_uri, request } = queryStringParameters;
+  if (request) {
+    const decoded = decodeJwt(request);
+    mockNonce = decoded.nonce;
+    mockState = decoded.state;
+    mockRedirectUri = decoded.redirect_uri;
+  } else {
+    mockNonce = nonce;
+    mockRedirectUri = redirect_uri;
+    mockState = state
+  }
   const scenarios = Object.keys(userScenarios)
     .map((scenario) => {
       return `<button name="scenario" value="${scenario}">${scenario}</button>`;
@@ -95,9 +105,9 @@ export const selectScenarioHandler = async (event: APIGatewayProxyEvent) => {
 
   const body = `<html><body>
       <form method="post" action='/authorize'>
-        <input type="hidden" name="state" value="${state}" />
-        <input type="hidden" name="nonce" value="${nonce}" /> 
-        <input type="hidden" name="redirectUri" value="${redirect_uri}" />
+        <input type="hidden" name="state" value="${mockState}" />
+        <input type="hidden" name="nonce" value="${mockNonce}" /> 
+        <input type="hidden" name="redirectUri" value="${mockRedirectUri}" />
         <h1>API Simulation Tool</h1>
         <p>Choose a scenario below instead of logging in. The app will act like you're that user, helping you test its behavior in different situations.</p>
         ${scenarios}
